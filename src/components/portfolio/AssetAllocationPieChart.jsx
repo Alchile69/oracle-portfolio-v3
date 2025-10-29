d55d6320caa67391523eb66e1e5dd701202cf102
Alchile69 <43197139+Alchile69@users.noreply.github.com>
2025-10-29 10:59:14 +0100

    Fix color lightening by adjusting green component

--- src/components/portfolio/AssetAllocationPieChart.jsx
+++ src/components/portfolio/AssetAllocationPieChart.jsx
@@ -117,7 +117,7 @@ const colors = [
     const num = parseInt(color.replace("#", ""), 16);
     const amt = Math.round(2.55 * percent);
     const R = (num >> 16) + amt;
-    const G = ((num >> 8) & 0x00FF);
+    const G = ((num >> 8) & 0x00FF) + amt;
     const B = (num & 0x0000FF) + amt;
     return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
       (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 +
